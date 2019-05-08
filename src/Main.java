1d3c4a0d25184e5db70891b1d1e3ef70afe41bdd
JoannaRatajczak <joanna.jr.ratajczak@gmail.com>
2019-05-08 22:15:23 +0200

    Kalkulator

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Math math = new Math();
+        System.out.println(math.isEven(5));
+        System.out.println(math.isOdd(5));
+        System.out.println(math.power(5));
+        System.out.println(math.circleField(5.5));
+    }
+}
---
+++ src/Math.java
@@ -0,0 +1,19 @@
+public class Math {
+
+    boolean isEven(int number) {
+        return (number%2==0);
+    }
+
+    boolean isOdd(int number) {
+        return (number%2!=0);
+
+    }
+    int power (int number) {
+        return number*number;
+    }
+
+    double circleField(double r){
+        return 3.14*(r*r);
+    }
+
+}
